7dc3f12a7e7d72c22b16c3a7967c93d638371767
Duong Thai Ngoc <thaingoctn5703@gmail.com>
2024-05-14 18:40:54 +0700

    check-thu-devtools

---
+++ Spring_3/src/test/java/ngoc/shop/spring/PropertiesConfig/PropertiesConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ngoc.shop.spring.PropertiesConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
